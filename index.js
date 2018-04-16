6ed0d0c6529ba17de57e153d0aaefb8d2251e6f7
Benny Skogberg <benny.skogberg@gmail.com>
2018-04-16 12:30:55 +0200

    Starting with XML 2 JSON parsing

--- index.js
+++ index.js
@@ -1,39 +1,54 @@
-const MongoClient = require('mongodb').MongoClient
-const http = require('http')
-const hostname = '127.0.0.1'
-const port = 2018
+// const MongoClient = require('mongodb').MongoClient
+const Parser = require('xml2json')
 
-// Start server
-const server = http.createServer((requset, response) => {
-  response.statusCode = 200
-  response.setHeader('Content-Type', 'text/plain')
-  response.end('Hello World\n')
-})
+var xml = '<foo attr=\'value\'>bar</foo>'
+console.log('input -> %s', xml)
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
-})
+// xml to json
+var json = Parser.toJson(xml)
+console.log('to json -> %s', json)
+
+// json to xml
+xml = Parser.toXml(json)
+console.log('back to xml -> %s', xml)
 
 // Connect to the db
+/*
 MongoClient.connect('mongodb://localhost:27017', (error, client) => {
   if (error) throw error
 
   var db = client.db('foodData')
 
-  db.collection('foodData', function (error, collection) {
+  db.collection('foodItems', function (error, collection) {
     if (error) throw error
 
     collection.insert({
-      id: 1,
+      Nummer: 1,
       Namn: 'Talg nöt',
       ViktGram: 100,
-      kcal: 656.3
+      Energi: {
+        Varde: 656.3,
+        Enhet: 'kcal'
+      },
+      Kolhydrater: {
+        Varde: 0.00,
+        Enhet: 'g'
+      },
+      Protein: {
+        Varde: 7.00,
+        Enhet: 'g'
+      },
+      Fett: {
+        Varde: 71.0,
+        Enhet: 'g'
+      }
     })
 
-    db.collection('foodData').count(function (err, count) {
+    db.collection('foodItems').count(function (err, count) {
       if (err) throw err
 
-      console.log('foodData: ' + count)
+      console.log('foodItems: ' + count)
     })
   })
 })
+*/
