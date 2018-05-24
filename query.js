6cffa92fcc44b1f149bc07cd73c9c77b39168c4b
Benny Skogberg <benny.skogberg@gmail.com>
2018-05-24 08:52:39 +0200

    Making a wildcard search from variable in MongoDB

--- query.js
+++ query.js
@@ -1,43 +1,36 @@
 const MongoClient = require('mongodb').MongoClient
 
 /*
-MongoClient.connect('mongodb://localhost:27017', (error, client) => {
-  if (error) throw error
-  var db = client.db('foodData')
-  // console.log(client)
-  db.collection('foodItems', function (error, collection) {
-    if (error) throw error
-    console.log('Upper: ' + collection)
-    var fooditem = collection.find({ Nummer: 1 })
-    fooditem.forEach(function (doc) {
-      // console.log(doc)
-      // process.exit()
-    }, function (err) {
-      if (err) throw err
-    })
-  })
-})
+From StackOverflow
+https://stackoverflow.com/a/494122/286244
+  This is because, although "." is a String, in the RegExp constructor
+  it's still interpreted as a regular expression, meaning any non-line-break
+  character, meaning every character in the string. For this purpose, the
+  following function may be useful:
 */
+RegExp.quote = function (str) {
+  return str.replace(/([.?*+^$[\]\\(){}|-])/g, '\\$1')
+}
 
 var url = 'mongodb://localhost:27017/foodData'
 MongoClient.connect(url)
-  .then(function (db) { // <- db as first argument
+  .then(function (db) {
     var foodDb = db.db('foodData')
     foodDb.collection('foodItems', function (error, collection) {
       if (error) throw error
-      // find
-      // var querystring = 'olja'
-      // var fooditem = collection.find({ Namn: /.*querystring.*/ })
-      var fooditem = collection.find({ Namn: /.*olja.*/ })
-      fooditem.forEach(function (item) {
-        console.log(item)
-        // process.exit()
-      })
 
-      // findOne - returns the first occurrance of Namn containing 'fett'
-      collection.findOne({ Namn: /.*fett.*/ }, function (error, document) {
-        if (error) throw error
-        // console.log(document)
+      // The users search string. Beware of capitaization.
+      var searchstring = '.'
+
+      // Make the searchstring into a wildcard search working in MongoDB
+      var searchFor = new RegExp(RegExp.quote(searchstring), 'g')
+
+      // Find all items containing searchstring stored as a RegExp in variable searchFor
+      var fooditem = collection.find({ Namn: searchFor })
+
+      // console.log all items containing searchstring
+      fooditem.forEach(function (item) {
+        console.log(item.Namn)
       })
     })
   })
